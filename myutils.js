889bc4b66e10b257ffef5bc9d4d4933073b0cdd3
animabear <whuxjj92@gmail.com>
2014-12-26 12:04:31 +0800

    add compare

--- myutils.js
+++ myutils.js
@@ -2,6 +2,7 @@ var utils = {
     $: function(id) {
         return document.getElementById(id);
     },
+
     /* 绑定函数作用域 */
     bind: function(fn, context) {
         var self = this;
@@ -12,7 +13,23 @@ var utils = {
             return fn.apply(context, arguments);
         }
     },
+
     isFunction: function(value) {
         return Object.prototype.toString.call(value) === '[object Function]';
+    },
+
+    // 对象数组排序传参  aObj.sort(key)
+    compare: function(key) {
+        return function (obj1, obj2) {
+            var value1 = obj1[key],
+                value2 = obj2[key];
+            if (value1 < value2) {
+                return -1;
+            } else if (value1 > value2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
     }
 }
